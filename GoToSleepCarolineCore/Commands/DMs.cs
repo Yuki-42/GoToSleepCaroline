40add2c5dc49e59a6ca787097bdab9358138622c
CamelGamer123 <kylepannan@gmail.com>
2023-10-16 14:45:13 +1000

    Completed DM create commands

--- GoToSleepCarolineCore/Commands/DMs.cs
+++ GoToSleepCarolineCore/Commands/DMs.cs
@@ -1,10 +1,10 @@
-﻿using DisCatSharp.ApplicationCommands;
+﻿using System.Globalization;
+using DisCatSharp.ApplicationCommands;
 using DisCatSharp.ApplicationCommands.Attributes;
 using DisCatSharp.ApplicationCommands.Context;
-using DisCatSharp.Common.Utilities;
 using DisCatSharp.Entities;
 using DisCatSharp.Enums;
-using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
 
 namespace GoToSleepCaroline.Commands;
 
@@ -24,11 +24,76 @@ public class DMs : ApplicationCommandsModule
                 // Create the command options
                 [Option("user", "The user to send the DM to.")] DiscordUser user,
                 [Option("message", "The message to send.")] string message,
-                [Option("time", "The time at which the DM will be sent.")] string time,
-                [Option("date", "The date on which the DM will be sent.")] string date
+                [Option("time", "The time at which the DM will be sent. Formatted as 'h:mm tt'")] string timeS,
+                [Option("date", "The date on which the DM will be sent. Formatted as 'dd MM yyyy'")] string dateS
                 )
             {
-                Console.WriteLine(CommandDatabase.Users);
+                // Check if the user exists in the database
+                if (!CommandDatabase.CheckUserExists(context.User.Id))
+                {
+                    CommandDatabase.AddUser(context.User.Id, context.User.Username, context.User.GlobalName, null, null);
+                }
+                
+                // Check that the user to send the DM to exists in the database
+                if (!CommandDatabase.CheckUserExists(user.Id))
+                {
+                    CommandDatabase.AddUser(user.Id, user.Username, user.GlobalName, null, null);
+                }
+                
+                // Parse the time and date  
+                DateOnly date = DateOnly.ParseExact(dateS,  "dd MM yyyy", CultureInfo.InvariantCulture);
+                TimeOnly time = TimeOnly.ParseExact(timeS, "h:mm tt", CultureInfo.InvariantCulture);
+                
+                // Create the action
+                CommandDatabase.AddAction(context.User.Id, 1, JObject.FromObject(new
+                {
+                    target = user.Id,
+                    messageText = message
+                }), time, date, false);
+                
+                // Send the confirmation message
+                await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                {
+                    Content = $"Created a scheduled DM to {user.Mention} at {time.ToString()} on {date.ToString()}."
+                });
+            }
+            
+            [SlashCommand("repeat", "Creates a scheduled DM that will be sent repeatedly.")]
+            public async Task Repeat(
+                InteractionContext context, 
+                // Create the command options
+                [Option("user", "The user to send the DM to.")] DiscordUser user,
+                [Option("message", "The message to send. Formatted as 'h:mm tt'")] string message,
+                [Option("time", "The time at which the DM will be sent. Formatted as 'dd MM yyyy'")] string time
+            )
+            {
+                // Check if the user exists in the database
+                if (!CommandDatabase.CheckUserExists(context.User.Id))
+                {
+                    CommandDatabase.AddUser(context.User.Id, context.User.Username, context.User.GlobalName, null, null);
+                }
+                
+                // Check that the user to send the DM to exists in the database
+                if (!CommandDatabase.CheckUserExists(user.Id))
+                {
+                    CommandDatabase.AddUser(user.Id, user.Username, user.GlobalName, null, null);
+                }
+                
+                // Parse the time and date  
+                TimeOnly timeOnly = TimeOnly.ParseExact(time, "h:mm tt", CultureInfo.InvariantCulture);
+                
+                // Create the action
+                CommandDatabase.AddAction(context.User.Id, 2, JObject.FromObject(new
+                {
+                    target = user.Id,
+                    messageText = message
+                }), timeOnly, null, true);
+                
+                // Send the confirmation message
+                await context.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+                {
+                    Content = $"Created a scheduled DM to {user.Mention} at {timeOnly.ToString()} every day."
+                });
             }
         }
     }
